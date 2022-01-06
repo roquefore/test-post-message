7140e60d0da4b8673d572b42aedae7ae0776d4a5
roquefore <roquefore@gmail.com>
2022-01-06 17:38:49 +0100

    qweqweqw

--- src/App.js
+++ src/App.js
@@ -29,12 +29,7 @@ function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <img
-                    src={logo}
-                    className='App-logo'
-                    alt='logo'
-                    onClick={() => applyForce(!force)}
-                />
+                <img src={logo} className='App-logo' alt='logo' />
                 <p
                     style={{
                         background: message || 'white',
@@ -42,14 +37,9 @@ function App() {
                 >
                     Edit <code>{message}</code> and save to reload.
                 </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
+                <p className='App-link' onClick={() => applyForce(!force)}>
                     Learn React
-                </a>
+                </p>
             </header>
         </div>
     );
